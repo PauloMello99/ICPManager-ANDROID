556fa3d1ec2eea48b4480988d0d7c3b392c46c8b
Roveri <gabriel.roveri@stoom.com.br>
2019-08-31 13:39:31 -0300

    StepView layout and class added, ProjectActivity modified

--- app/src/main/java/br/com/fatec/icpmanager/activity/ProjectActivity.java
+++ app/src/main/java/br/com/fatec/icpmanager/activity/ProjectActivity.java
@@ -1,20 +1,22 @@
 package br.com.fatec.icpmanager.activity;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 import android.util.Log;
-import android.view.View;
-import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.ValueEventListener;
+import com.shuhart.stepview.StepView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import br.com.fatec.icpmanager.R;
 import br.com.fatec.icpmanager.model.Project;
@@ -27,13 +29,18 @@ public class ProjectActivity extends AppCompatActivity {
     TextView startTextView;
     TextView endTextView;
     ProgressBar progressBar;
+    StepView stepView;
+
+    private int currentStep = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_project);
         setComponents();
+        stepAction();
     }
+
 
     private void setComponents() {
         getProject();
@@ -46,6 +53,7 @@ public class ProjectActivity extends AppCompatActivity {
         startTextView = findViewById(R.id.start_date_project);
         endTextView = findViewById(R.id.end_date_project);
         progressBar = findViewById(R.id.progress_project);
+        stepView = findViewById(R.id.step_view);
     }
 
     private void getProject() {
@@ -74,6 +82,35 @@ public class ProjectActivity extends AppCompatActivity {
                 finish();
             }
         });
+    }
+
+    private void stepAction(){
+
+        //FIXME ANIMAÇÃO DO STEP ESTÁ MUITO LENTA (PODE SER O EMULADOR)
+        //TODO ADICIONAR TEXTO UNICO PARA CADA FASE
+        stepView.setOnStepClickListener(step -> Toast.makeText(ProjectActivity.this, "Step " + step, Toast.LENGTH_SHORT).show());
+        findViewById(R.id.next).setOnClickListener(v -> {
+            if (currentStep < stepView.getStepCount() - 1) {
+                currentStep++;
+                stepView.go(currentStep, true);
+            } else {
+                stepView.done(true);
+            }
+        });
+        findViewById(R.id.back).setOnClickListener(v -> {
+            if (currentStep > 0) {
+                currentStep--;
+            }
+            stepView.done(false);
+            stepView.go(currentStep, true);
+        });
+        List<String> steps = new ArrayList<>();
+        for (int i = 0; i < 5; i++) {
+            steps.add("Step " + (i + 1));
+        }
+        steps.set(steps.size() - 1, steps.get(steps.size() - 1) + " last one");
+        stepView.setSteps(steps);
+
     }
 
     @Override
